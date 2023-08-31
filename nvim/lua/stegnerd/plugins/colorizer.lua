fb5fc83f687756c5eba5633e4830a840edfe203e
Dalton Stegner <dmstegner@gmail.com>
2023-08-30 22:24:31 -0400

    colorizer amd gitsigns

---
+++ nvim/lua/stegnerd/plugins/colorizer.lua
@@ -0,0 +1,7 @@
+-- used to specify color  previews
+-- or specify colors for other properties
+return {
+  "NvChad/nvim-colorizer.lua",
+  event = { "BufReadPre", "BufNewFile" },
+  config = true,
+}
