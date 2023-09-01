3262909fcf074e3e11c8e7690180d2b7744f3029
Dalton Stegner <dmstegner@gmail.com>
2023-08-31 23:47:16 -0400

    tmux navigator

--- nvim/lua/stegnerd/plugins/init.lua
+++ nvim/lua/stegnerd/plugins/init.lua
@@ -1,4 +1,5 @@
 return {
 -- these  are plugins that will load all the time with no configs
- "nvim-lua/plenary.nvim", -- lua functions that many plugins use	
+ "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+ -- "christoomey/vim-tmux-navigator", -- tmux & split window navigation
 }
