e6e590adadbcb9c1ef5654b7daec6ef801b0e573
Dalton Stegner <dmstegner@gmail.com>
2024-05-26 13:50:29 -0400

    bump tmux nav and fix hack

--- nvim/lua/stegnerd/core/keymaps.lua
+++ nvim/lua/stegnerd/core/keymaps.lua
@@ -1,8 +1,5 @@
 vim.g.mapleader = " "
 
--- https://github.com/christoomey/vim-tmux-navigator/pull/392
--- https://github.com/christoomey/vim-tmux-navigator/pull/393
-vim.g.Netrw_UserMaps = { { "<C-l>", "<C-U>TmuxNavigateRight<cr>" } }
 
 local keymap = vim.keymap -- for conciseness
 
@@ -25,5 +22,3 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })                     --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                 --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-
-keymap.del("n", "<c-l>")
